9bf86463132f20e9b8145a59ef8471a57670fb53
Marc Fiume <mfiume@cs.toronto.edu>
2013-09-22 11:19:43 -0400

    Scanning barcodes

---
+++ v1/OandB/ScanViewController.h
@@ -0,0 +1,16 @@
+//
+//  ScanViewController.h
+//  Bora
+//
+//  Created by Marc Fiume on 2013-09-22.
+//  Copyright (c) 2013 Marc Fiume. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ZBarSDK.h"
+
+@interface ScanViewController : UIViewController<ZBarReaderDelegate>
+
+- (IBAction)startScanning:(id)sender;
+
+@end
---
+++ v1/OandB/ScanViewController.m
@@ -0,0 +1,70 @@
+//
+//  ScanViewController.m
+//  Bora
+//
+//  Created by Marc Fiume on 2013-09-22.
+//  Copyright (c) 2013 Marc Fiume. All rights reserved.
+//
+
+#import "ScanViewController.h"
+
+@interface ScanViewController ()
+
+@end
+
+@implementation ScanViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)startScanning:(id)sender {
+    
+    NSLog(@"Scanning..");
+
+    ZBarReaderViewController *codeReader = [ZBarReaderViewController new];
+    codeReader.readerDelegate=self;
+    codeReader.supportedOrientationsMask = ZBarOrientationMaskAll;
+    
+    ZBarImageScanner *scanner = codeReader.scanner;
+    [scanner setSymbology: ZBAR_I25 config: ZBAR_CFG_ENABLE to: 0];
+    
+    [self presentViewController:codeReader animated:YES completion:nil];
+}
+
+- (void) imagePickerController: (UIImagePickerController*) reader didFinishPickingMediaWithInfo: (NSDictionary*) info
+{
+    //  get the decode results
+    id<NSFastEnumeration> results = [info objectForKey: ZBarReaderControllerResults];
+    
+    ZBarSymbol *symbol = nil;
+    for(symbol in results)
+        // just grab the first barcode
+        break;
+    
+    // showing the result on textview
+    NSLog(@"%@",symbol.data);
+    
+    //resultImageView.image = [info objectForKey: UIImagePickerControllerOriginalImage];
+    
+    // dismiss the controller
+    [reader dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
