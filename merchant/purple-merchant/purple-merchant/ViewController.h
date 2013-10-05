a0da409baaac87e4170dd6baa112a6a8b321f354
Marc Fiume <mfiume@cs.toronto.edu>
2013-10-05 15:44:55 -0400

    Merchant added

---
+++ merchant/purple-merchant/purple-merchant/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  purple-merchant
+//
+//  Created by Marc Fiume on 2013-10-05.
+//  Copyright (c) 2013 Marc Fiume. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
